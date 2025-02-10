24c3b3d62fd2b080223ff5c8d239347ac3db2e20
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-10 22:35:30 +0300

    swagger eklendi.

--- BlogProject.WebApi/Program.cs
+++ BlogProject.WebApi/Program.cs
@@ -11,8 +11,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+   
+    app.UseSwagger();
+    app.UseSwaggerUI();
     
-    app.SwaggerUý();
 }
 
 app.UseHttpsRedirection();
