a485de63ea57b47b0cadff80a55c74aca011df0a
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-11 12:18:21 +0300

    CONTROLLER.

---
+++ BlogProject.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿using BlogProject.WebApi.Abstractions;
+using MediatR;
+
+namespace BlogProject.WebApi.Controllers;
+
+
+public class ValuesController : ApiController
+{
+    public ValuesController(IMediator mediator) : base(mediator)
+    {
+    }
+}
