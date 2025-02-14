f4e497194fbae2c3254113404ab171d2b4010b04
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-14 20:37:15 +0300

    ....

--- BlogProject.Infrastructure/Migrations/20250214173539_mgf78s3434SAS787sas.Designer.cs
+++ BlogProject.Infrastructure/Migrations/20250214173539_mgf78s3434SAS787sas.Designer.cs
@@ -4,6 +4,7 @@ using BlogProject.Infrastructure.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogProject.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250214173539_mgf78s3434SAS787sas")]
+    partial class mgf78s3434SAS787sas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
