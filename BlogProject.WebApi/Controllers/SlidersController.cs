dc7c0545ac2af520c7f3e6882aa3be76185215b5
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-14 14:09:10 +0300

    Sliders Controller

---
+++ BlogProject.WebApi/Controllers/SlidersController.cs
@@ -0,0 +1,14 @@
+﻿
+
+using BlogProject.WebApi.Abstractions;
+using MediatR;
+
+namespace BlogProject.WebApi.Controllers
+{
+    public class SlidersController : ApiController
+    {
+        public SlidersController(IMediator mediator) : base(mediator)
+        {
+        }
+    }
+}
