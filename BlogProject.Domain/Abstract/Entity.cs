8ed0972ef15d02149182c9d69416747d0adaee7a
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-14 17:10:12 +0300

    BASE CLASS

---
+++ BlogProject.Domain/Abstract/Entity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BlogProject.Domain.Abstract
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
