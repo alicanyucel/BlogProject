c6033a4bcef96d8f42357571449b3351e3f91db1
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-16 16:44:42 +0300

    açıklama satırı ayzıldı....

--- BlogProject.Domain/Entities/Language.cs
+++ BlogProject.Domain/Entities/Language.cs
@@ -8,6 +8,7 @@ public class Language : IEntity
     [Key]
     public Guid LanguageId { get; set; }
     public Guid RowNumber { get; set; }
-    public List<Slider> Sliders { get; set; }
+    public List<Slider> Sliders { get; set; } // bu değerler language tablosunda gozukmeyecektir sadece ilşkili olduugnu soyluyor
+
 
 }
